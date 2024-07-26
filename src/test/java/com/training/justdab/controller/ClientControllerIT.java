5d6dc42caf6ba611848663434d372158a689ceab
Anliat MOHAMED <anliatmohamed@gmail.com>
2024-07-26 23:15:30 +0200

    update testcontainer in controllerIT

--- src/test/java/com/training/justdab/controller/ClientControllerIT.java
+++ src/test/java/com/training/justdab/controller/ClientControllerIT.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -21,6 +23,13 @@ public class ClientControllerIT {
     @Container
     private static final MySQLContainer<?> SQL_CONTAINER = new MySQLContainer<>()
             .withDatabaseName("just_dab_db");
+
+    @DynamicPropertySource
+    static void configureMySqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", SQL_CONTAINER::getPassword);
+    }
 
     @Autowired
     private MockMvc mockMvc;
